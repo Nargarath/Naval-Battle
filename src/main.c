77968226d192ac13538ccd43a25dab04dbc68c4f
Yuri Alessandro <yuri.alessandro@hotmail.com>
2016-10-20 19:19:26 -0300

    Project organization

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include "../lib/ia.h"
+#include "../lib/play1.h"
+#include "../lib/jogar.h"
+
+// Chama função menu, que contém o loop para permanecer no jogo até que o usuário queira sair.
+int main(){	
+	srand(time(NULL));
+	menu();
+	return 0;
+}
